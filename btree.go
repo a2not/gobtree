7259c30dbadc4b76d3290cf90c60ed154a3c87a7
Khdbble <fb503d52.af1c4d@gmail.com>
2021-06-03 16:44:59 +0900

    added BTree, node, Item types

--- btree.go
+++ btree.go
@@ -1,3 +1,16 @@
 package gobtree
 
+type Item interface {
+	Less(other Item) bool
+}
+
+type node struct {
+	items    []Item
+	children []*node
+}
+
+type BTree struct {
+	degree int
+	length int
+	root   *node
 }
