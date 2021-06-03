1146ec7eae7f0dbbf34fde9ea49557bad688a43d
Khdbble <fb503d52.af1c4d@gmail.com>
2021-06-03 20:32:33 +0900

    added minItems(), maxItems()

--- btree.go
+++ btree.go
@@ -11,7 +11,7 @@ type node struct {
 
 type BTree struct {
 	degree int
-	length int
+	size   int // number of elements in a whole tree
 	root   *node
 }
 
@@ -19,4 +19,14 @@ func New(degree int) *BTree {
 	return &BTree{
 		degree: degree,
 	}
+}
+
+// maximum number of items in a node
+func (t *BTree) maxItems() int {
+	return t.degree*2 - 1
+}
+
+// minimum number of items in a node
+func (t *BTree) minItems() int {
+	return t.degree - 1
 }
